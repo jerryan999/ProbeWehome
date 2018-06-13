93712ca02122a66fe3e560810b5fb532469e3f55
Your Name <you@example.com>
2018-06-13 13:41:35 +0800

    fix shell

--- process.sh
+++ process.sh
@@ -5,6 +5,6 @@ i=1
 while read line; do    
     # line is available for processing
     echo Line $i : $line
-    phantomjs netlog.js $line    
+    ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs netlog.js $line    
     i=$(( i + 1 ))
 done < wehome_url.txt
