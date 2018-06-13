62bc9a36f2fc0877b57952865531c79d539216de
jerryan9999 <jerry.an@rentx.io>
2018-06-13 14:59:27 +0800

    add response time

--- netlog.js
+++ netlog.js
@@ -2,13 +2,14 @@
 var page = require('webpage').create(),
     system = require('system'),
     address;
+var last_response = null
 
 if (system.args.length === 1) {
     console.log('Usage: netlog.js <some URL>');
     phantom.exit(1);
 } else {
     address = system.args[1];
-
+    console.log("Started at:"+Date())
     console.log(address)
     page.onResourceRequested = function (req) {
         //console.log('requested: ' + JSON.stringify(req, undefined, 4));
@@ -16,12 +17,14 @@ if (system.args.length === 1) {
 
     page.onResourceReceived = function (res) {
         //console.log('received: ' + JSON.stringify(res, undefined, 4));
+        last_response = res
     };
 
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('FAIL to load the address');
 	 }
+     console.log("Finished at:"+last_response.time)
      phantom.exit();
     });
 }
